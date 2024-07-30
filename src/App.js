8bea579c60d54d326f1f6456010a1d60d4a4124c
Challe <you@example.com>
2024-07-30 17:41:39 +0200

    Configure backend URL for frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+const apiUrl = process.env.REACT_APP_BACKEND_URL;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+fetch(`${apiUrl}/api/some-endpoint`)
+  .then(response => response.json())
+  .then(data => console.log(data));
