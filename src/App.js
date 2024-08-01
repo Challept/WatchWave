2e785dcd6f69345b6f6de085b3842e512a684620
Challe <you@example.com>
2024-08-01 20:38:31 +0200

    Deploy updated frontend build

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import RecentWatched from './pages/RecentWatched';
 import './App.css';
-import Container from '@mui/material/Container';
 
 function App() {
   return (
     <Router>
       <NavBar />
-      <Container>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recent-watched" element={<RecentWatched />} />
-          </Routes>
-        </div>
-      </Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recent-watched" element={<RecentWatched />} />
+      </Routes>
     </Router>
   );
 }
